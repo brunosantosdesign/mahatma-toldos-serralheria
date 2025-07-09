608384ae1535df6751d46c9a9ec9c771045fca6f
brunosantosdesign <bruno.s.araujo88@gmail.com>
2025-07-09 17:47:40 -0300

    projeto mahatma serralheria adicionado

---
+++ js/main.js
@@ -0,0 +1,151 @@
+// Button Whatss
+const Whats = document.querySelector("#Whats")
+
+Whats.addEventListener("click", () => {
+  window.open(
+    "https://api.whatsapp.com/send?phone=5513982323400&text=%5BN%C3%A3o%20Apague-Google%20Ads%5D%20Ol%C3%A1,%20gostaria%20de%20fazer%20um%20or%C3%A7amento!",
+    "_blank"
+  )
+})
+
+// ANIMATE AOS
+AOS.init({
+  duration: 1000,
+  once: true,
+})
+
+
+// Ativar Links do Menu
+// const links = document.querySelectorAll(".header-menu a")
+
+// function ativarLink(link) {
+//   const url = location.href
+//   const href = link.href
+//   if (url.includes(href)) {
+//     link.classList.add("ativo")
+//   }
+// }
+
+// links.forEach(ativarLink)
+
+
+// Scroll suave para link interno
+$('ul a').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('nav').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+// Debounce do Lodash
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+// Animação ao Scroll
+(function(){
+	var $target = $('.anime'),
+			animationClass = 'anime-start',
+			offset = $(window).height() * 3/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 200));
+})();
+
+
+
+// BTN MOBILE
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+	const right = document.getElementById('right');
+	right.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
+
+
+// Formulario
+
+$('.formphp').on('submit', function() {
+	var emailContato = "contato@mahatmatoldos.com.br"; // Escreva aqui o seu e-mail
+
+	var that = $(this),
+			url = that.attr('action'),
+			type = that.attr('method'),
+			data = {};
+	
+	that.find('[name]').each(function(index, value) {
+		var that = $(this),
+				name = that.attr('name'),
+				value = that.val();
+				
+		data[name] = value;
+	});
+	
+	$.ajax({
+		url: url,
+		type: type,
+		data: data,
+		success: function(response) {
+		
+			if( $('[name="leaveblank"]').val().length != 0 ) {
+				$('.formphp').html("<div id='form-erro'></div>");
+				$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-erro');
+				});
+			} else {
+			
+				$('.formphp').html("<div id='form-send'></div>");
+				$('#form-send').html("<span>Mensagem enviada!</span><p>Em breve eu entro em contato com você. Abraços.</p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-send');
+				});
+			};
+		},
+		error: function(response) {
+			$('.formphp').html("<div id='form-erro'></div>");
+			$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+			.hide()
+			.fadeIn(1500, function() {
+			$('#form-erro');  
+		});
+		}
+	});
+	
+	return false;
+});
